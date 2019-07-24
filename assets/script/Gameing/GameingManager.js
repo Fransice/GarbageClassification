c83491d976a9dd0cf8d6d9b648a6e86149c334d2
Fransic <1329030364@qq.com>
2019-07-24 10:13:47 +0800

    最终版

--- assets/script/Gameing/GameingManager.js
+++ assets/script/Gameing/GameingManager.js
@@ -76,10 +76,8 @@ cc.Class({
     },
     //返回主界面
     backHome() {
-        var re = new RegExp("/^[0-9]+.?[0-9]*/");//判断字符串是否为数字//判断正整数/[1−9]+[0−9]∗]∗/ 
-        if (!re.test(this._InputOut.string) && this._InputOut.string.length != 11) {
-            alert("请正确输入号码");
-        } else {
+        var re = /^1[3456789]\d{9}$/;//判断字符串是否为数字//判断正整数/[1−9]+[0−9]∗]∗/ 
+        if (re.test(this._InputOut.string)) {
             var data = {
                 "rightCount": this._nowscore,
                 "wrongCount": this._wrongCount,
@@ -93,8 +91,9 @@ cc.Class({
             this._Success_UI.active = false;
             this._Result_UI.active = false;
             this._Gameing_UI.active = false;
+        } else {
+            alert("请正确输入号码");
         }
-
     },
     Init() {
         this._Gameing_UI.active = true;
